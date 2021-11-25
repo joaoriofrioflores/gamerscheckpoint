cdfb13ed414b4cb94ac8ca6bd768674590e8bfa1
JeanJaramillo <81722915+JeanJaramillo@users.noreply.github.com>
2021-11-25 12:11:46 -0500

    Puerto automático || 3000 y prueba de index ejs

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const path = require('path')
 const express =require('express')
 const logger = require('morgan')
-const bodyParser = require('body-parser')
+const bodyParse = require('body-parser')
+const app = express();
+const indexRoutes = require('./src/routes/index')
 
-const app=express();
-const { urlencoded } = require('express');
-const indexRouters = require('./index')
-
-app.set('port', 3000)
-app.set('views', __dirname)
+app.set('port', process.env.PORT ||3000)
+app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
-app.use(bodyParser,urlencoded({extender:false}))
+app.use(express.json())
+app.use(bodyParse.urlencoded({extended:false}))
 
-app.listen(app.get('port'), ()=>{
-    console.log("SERVIDOR ACTIVADO Y EJECUTANDOSE")
+app.use('/', indexRoutes)
+
+app.listen(app.get('port'),()=>{
+    console.log("SERVIDOR EN PUERTO", app.get('port'))
 })
--- index.js
+++
@@ -1,9 +0,0 @@
-const express =require('express');
-const router = express.Router();
-const client = require('./db');
-
-router.get('/', (req, res)=> {
-    res.render('main')
-})
-
-module.exports = router;
