4c5fec9208e9b2e66979cd398527725172f2a59d
JeanJaramillo <81722915+JeanJaramillo@users.noreply.github.com>
2021-11-26 01:34:09 -0500

    Trabajando la db de dirección de entrega

--- venta-direccion.py
+++ venta-direccion.py
@@ -1,1 +1,33 @@
 #trabajando...
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://jean-rafael:lokazo.420@clustercertus.6mvum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+db = client.db_gcp
+
+coll = db.users
+
+coll.insert_many([
+    {'nombre':'Lokazo thc',
+    'apellidos':'Cuatro Veinte',
+    'provincia':'Lima',
+    'distrito':'Breña',
+    'calle':'Jr. Marhihuana',
+    'numero':'420',
+    'interior':'666',
+    'telefonos':['992453123', '929351202']},
+    {'nombre':'Anacleto Cocaina',
+    'apellidos':'Sopota Madre',
+    'provincia':'Tacna',
+    'distrito':'Miraflores',
+    'calle':'Jr. LSD',
+    'numero':'69',
+    'interior':'420',
+    'telefonos':['999555333', '988977344']}
+])
+
+for documento in coll.find({
+
+})
+
+print(client.list_database_names())
